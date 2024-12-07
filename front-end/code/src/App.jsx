3c49173938667b4e002e1b15d82b2795d03912f8
ELKAMAL Mohammed <elkamalmohammed234@gmail.com>
2024-12-07 11:54:01 +0000

    Sat Dec  7 11:54:01 UTC 2024

--- front-end/code/src/App.jsx
+++ front-end/code/src/App.jsx
@@ -7,10 +7,11 @@ import Blog from './components/Blog/Blog';
 import Contact from './components/Contact/Contact';
 import Recipe_details from './components/Recipe/Recipe_details';
 import NotFound from './components/NotFound';
-
+import { SpeedInsights } from "@vercel/speed-insights/react"
 function App() {
   return (
       <main className="px-40 flex flex-col gap-20">
+        <SpeedInsights />
         <Nav />
         <Routes>
           <Route path="/" element={<Home />} />
